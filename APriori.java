139c3f2e337c3c73e06722bca190c2375d7c8aac
BeaLove <67667545+BeaLove@users.noreply.github.com>
2020-11-15 09:51:30 +0100

    Edit to count method to combine only frequents

--- APriori.java
+++ APriori.java
@@ -1,35 +1,28 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-//import java.util.Arrays;
-//import java.util.Iterator;
+import java.util.*;
 
-public class APriori{
-    
+import com.google.common.collect.Sets;
+
+
+public class APriori {
     public APriori(){
 
     }
 
-    public HashMap<HashSet<Integer>, Integer> createSingletons(ArrayList<HashSet<Integer>> baskets) {
+    public HashMap<Integer, Integer> createSingletons(ArrayList<Set<Integer>> baskets) {
         //IN market basket model dataset, list of sets of integers (eg items purchased in transactions)
         //OUT HashMap of key-value pairs of each integer (item) and the number of times it appears in all baskets
-        HashMap<HashSet<Integer>, Integer> singletons = new HashMap<>();
+        HashMap<Integer, Integer> singletons = new HashMap<>();
         int num_baskets = baskets.size();
         System.out.println("num baskets: " + num_baskets);
-        for (int b = 0; b< num_baskets; b++){ //for each basket
-            HashSet<Integer> basket = baskets.get(b);
+        for (Set<Integer> basket : baskets) { //for each basket
             //System.out.println("current basket size: " + num_items);
-            for (int item: basket){ //for each item in each basket
-                //int item = baskets.get(b)[i];
-                //System.out.println(item);
-                //HashSet<Integer> singleton = new HashSet<>(baskets.get(b)[i]);
-                HashSet<Integer> singleton = new HashSet<>(); 
-                singleton.add(item); //create new hashset and add current item to it.
-                if (singletons.containsKey(singleton)){ //if we've already seen this item, increment its count by 1
-                    singletons.put(singleton, singletons.get(singleton) +1 );
-                }    
-                else {
-                    singletons.put(singleton, 1); //if item is new, count it once
+            for (int item : basket) { //for each item in each baske
+                //Set<Integer> singleton = Sets.newHashSet();
+                //singleton.add(item); //create new hashset and add current item to it.
+                if (singletons.containsKey(item)) { //if we've already seen this item, increment its count by 1
+                    singletons.put(item, singletons.get(item) + 1);
+                } else {
+                    singletons.put(item, 1); //if item is new, count it once
                 }
                 /*itemSets.entrySet().forEach( entry -> {
                     System.out.println( entry.getKey() + " => " + entry.getValue() );
@@ -39,61 +32,66 @@ public class APriori{
         return singletons;
     }
 
-    public HashMap<HashSet<Integer>, Integer> pruning(HashMap<HashSet<Integer>, Integer> map, int threshold){
-        //IN hashmap of items and counts 
+    public HashMap<Integer, Integer> prune_singles(HashMap<Integer, Integer> map, int threshold){
+        //IN hashmap of items and counts
         //OUT pruned hashmap of only items that appear at least "threshold" times
         map.entrySet().removeIf(entry -> entry.getValue() < threshold); //removes all key-value pairs if value is below support threshold
         return map;
     }
-
-    public HashMap<HashSet<Integer>, Integer> mergeItemsets(HashMap<HashSet<Integer>, Integer> sets_counts) {
+    public HashMap<Set<Integer>, Integer> prune(HashMap<Set<Integer>, Integer> map, int threshold){
+        map.entrySet().removeIf(entry -> entry.getValue() < threshold); //removes all key-value pairs if value is below support threshold
+        return map;
+    }
+    /*public HashMap<Set<Integer>, Integer> mergeItemsets(HashMap<Integer, Integer> sets_counts) {
         //IN: HashMap of itemsets and counts, k = iteration round of A Priori
         //OUT: HashMap where keys have been merged ( k <-- k-1)
-        ArrayList<HashSet<Integer>> sets = new ArrayList<>(sets_counts.keySet());
-        HashMap<HashSet<Integer>, Integer> mergedSets = new HashMap<>();
-        for (HashSet<Integer> set: sets){
-            for (HashSet<Integer> otherset: sets){
+        //ArrayList<Set<Integer>> sets = new ArrayList<>(sets_counts.keySet());
+        HashMap<Set<Integer>, Integer> mergedSets = new HashMap<>();
+        for (Set<Integer> set: sets){
+            for (Set<Integer> otherset: sets){
                 if (set.equals(otherset)){
-                    continue;
                 }
                 else {
-                    HashSet<Integer> merge = new HashSet<>(set);
-                    merge.addAll(otherset);
+                    Set<Integer> merge = Sets.union(set, otherset);
                     mergedSets.put(merge, 0);
                 }
             }
         }
-        /*HashMap<HashSet<Integer>, Integer> mergedSets = new HashMap<>(); 
-        for (HashSet<Integer> set: sets.keySet()){
-            for (HashSet<Integer> otherset: sets.keySet()){
-                HashSet<Integer> merged = new HashSet<>();
-                merged.addAll(set);
-                merged.addAll(otherset);
-                //set.addAll(otherset);
-                mergedSets.put(set, 0);
-            }
-        }
-        mergedSets.entrySet().removeIf(entry -> entry.getKey().size() < k); //remove any k-1 sets remaining (addAll wont add a set to itself)
-        */
-        //mergedSets.entrySet().forEach( entry -> {System.out.println( "doubletons " + entry.getKey() + "=>" + entry.getValue());});
         return mergedSets;
-    }
+    }*/
 
-    public HashMap<HashSet<Integer>, Integer> counter(ArrayList<HashSet<Integer>> baskets, HashMap<HashSet<Integer>, Integer> itemsets){
+    public HashMap<Set<Integer>, Integer> counter(ArrayList<Set<Integer>> baskets, Set<Integer> frequent_singles, int k){
         //IN: list of baskets of items, HashMap of itemsets and their counts
         //OUT: HashMap of itemsets and zeroed counts
         //itemsets.replaceAll((key, value) -> value = 0); //set all counts from previous round to 0;
+        //Set<Integer> candidates = Sets.newHashSet();
+        HashMap<Set<Integer>, Integer> sets_counts = new HashMap<>();
         System.out.println("hello in counter");
-        for (HashSet<Integer> basket: baskets){
-            for (HashSet<Integer> itemset: itemsets.keySet()){
-                if (basket.containsAll(itemset)){
-                    itemsets.put(itemset, itemsets.get(itemset) +1);
+        for (Set<Integer> basket: baskets){
+            Set<Integer> candidates = Sets.newHashSet();
+            for (int item:basket) {
+                if (frequent_singles.contains(item)){
+                    candidates.add(item);
                 }
             }
+            try {
+                Set<Set<Integer>> combos = Sets.combinations(candidates, k);
+                for (Set<Integer> combo: combos){
+                        if (sets_counts.keySet().contains(combo)) {
+                            sets_counts.put(combo, sets_counts.get(combo) + 1);
+                        }
+                        else{
+                            sets_counts.put(combo, 1);
+                        }
+                    }
+
+            }
+            catch (Exception IllegalArgumentException){
+                continue;
+            }
+
         }
         System.out.println("done in counter");
-        return itemsets;
+        return sets_counts;
     }
-        
-    
 }
--- Driver.java
+++ Driver.java
@@ -1,78 +1,74 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.HashSet;
-import java.util.HashMap;
+import java.util.*;
 
-public class Driver{
-    //Constants
+import com.google.common.collect.Sets;
+
+
+public class Driver {
     private static final int THRESHOLD = 3;
 
     public static void main(String[] args) throws FileNotFoundException {
 
         String filename = "baskets.dat";
-        ArrayList<HashSet<Integer>> baskets = readFromFile(filename);
+        ArrayList<Set<Integer>> baskets = readFromFile(filename);
         int num_baskets = baskets.size();
         int THRESHOLD = num_baskets/100; //1% frequency threshold
         //int size_last_basket = baskets.get(num_baskets-1).;
         System.out.println("number baskets: " + num_baskets);
         //System.out.println("size of last basket: " + size_last_basket);
-        ArrayList<HashSet<Integer>> sub_baskets = new ArrayList<>();
-        for (int i= 0; i < 1000; i++){
-            sub_baskets.add(baskets.get(i));
-        }
         APriori apriori = new APriori();
-        HashMap<HashSet<Integer>, Integer> singletons = apriori.createSingletons(sub_baskets);
-        //debugging code prints all key value pairs!! 
+        HashMap<Integer, Integer> singletons = apriori.createSingletons(baskets);
+        //debugging code prints all key value pairs!!
         //singletons.entrySet().forEach( entry -> {System.out.println( entry.getKey() + " => " + entry.getValue() );});
         int size = singletons.size();
         System.out.println("singletons size: "+ size);
         //pruning removes all key-value pairs where value is below threshold
-        HashMap<HashSet<Integer>, Integer> pruned_singletons = apriori.pruning(singletons, 10);
-        System.out.println("after pruning: " + pruned_singletons.size());
-        System.out.println("original map after pruning: " + singletons.size());
-        //ArrayList<HashSet<Integer>> singletons_list = new ArrayList<>(singletons.keySet());
+        //HashMap<Set<Integer>, Integer> pruned_singletons = apriori.pruning(singletons, 10);
+        //System.out.println("after pruning: " + pruned_singletons.size());
+        //System.out.println("original map after pruning: " + singletons.size());
+        //ArrayList<Set<Integer>> singletons_list = new ArrayList<>(singletons.keySet());
         //PLACEHOLDER k for iteration count
         int k = 1;
-        HashMap<HashSet<Integer>, Integer> merged_sets = apriori.mergeItemsets(singletons);
-        HashMap<HashSet<Integer>, Integer> doubletons = apriori.counter(sub_baskets, merged_sets);
-        System.out.println("hello 1");
-        int doubles_size = doubletons.size();
-        System.out.println(doubles_size);
-        //doubletons.entrySet().forEach( entry -> {System.out.println( "doubletons " + entry.getKey() + "=>" + entry.getValue());});
-        System.out.println("hello 2");
-        HashMap<HashSet<Integer>, Integer> itemSets = singletons;
-        while (!itemSets.isEmpty()){
-            itemSets = apriori.pruning(itemSets, THRESHOLD);
-            itemSets = apriori.mergeItemsets(itemSets);
-            itemSets = apriori.counter(baskets, itemSets);
-            
-
-
+        //HashMap<Integer, Integer> merged_sets = apriori.mergeItemsets(singletons);
+        HashMap<Integer, Integer> pruned_singles = apriori.prune_singles(singletons, THRESHOLD);
+        //HashMap<Integer, Integer> itemSets = singletons;
+        Set<Integer> frequent_singles = Sets.newHashSet(singletons.keySet());
+        //XMLStreamWriterImpl.Element sets_counts;
+        HashMap<Set<Integer>, Integer> sets_counts;
+        do {
+            //itemSets = apriori.mergeItemsets(itemSets);
+            sets_counts = apriori.counter(baskets, frequent_singles, k);
+            sets_counts = apriori.prune(sets_counts, THRESHOLD);
+            for (Map.Entry itemSet:sets_counts.entrySet()){
+                System.out.println(itemSet);
+            }
+            k++;
         }
+        while (!sets_counts.isEmpty());
 
     }
 
-    public static ArrayList<HashSet<Integer>> readFromFile(String filename) throws FileNotFoundException {
+    public static ArrayList<Set<Integer>> readFromFile(String filename) throws FileNotFoundException {
         //IN filename
         //OUT market basket dataset ArrayList of sets of transactions with items represented as integers
-        ArrayList<HashSet<Integer>> baskets = new ArrayList<>();
-            // pass the path to the file as a parameter
-            File file = new File(filename);
-            Scanner sc = new Scanner(file);
-            while (sc.hasNextLine()){
-                String s = sc.nextLine();
-                String[] splitted = s.split(" ");
-                HashSet<Integer> basket = new HashSet<>();
-                //int [] arr = new int[splitted.length];
-                for (int i = 0; i < splitted.length; i++){
-                    basket.add(Integer.parseInt(splitted[i]));
-                }
-                
-                baskets.add(basket);
+        ArrayList<Set<Integer>> baskets = new ArrayList<>();
+        // pass the path to the file as a parameter
+        File file = new File(filename);
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()){
+            String s = sc.nextLine();
+            String[] splitted = s.split(" ");
+            Set<Integer> basket = Sets.newHashSet();
+            //int [] arr = new int[splitted.length];
+            for (String value : splitted) {
+                basket.add(Integer.parseInt(value));
             }
+
+            baskets.add(basket);
+        }
         sc.close();
         return baskets;
     }
 }
+
